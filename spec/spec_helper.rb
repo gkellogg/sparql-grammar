f44eebeb7baeeb9616b5d58576d922f41a80eb2e
Arto Bendiken <arto@bendiken.net>
2010-09-27 16:01:03 +0200

    Defined the DAWG test suite vocabularies for use in specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
 require 'sparql/grammar'
 require 'rdf/spec'
+require 'rdf/ntriples'
+require 'rdf/raptor'
 
 Spec::Runner.configure do |config|
   config.include(RDF::Spec::Matchers)
 end
+
+DAWG = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#')
+MF   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#')
+QT   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-query#')
+RS   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/result-set#')
