b505cee9999fe366ac151172c6567b89e43cc05f
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-01-09 22:53:31 -0800

    Added DAWG syntax specs based on Spira model of test manifests.

---
+++ spec/dawg_spec.rb
@@ -0,0 +1,25 @@
+$:.unshift "."
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe SPARQL::Grammar::Parser do
+  describe "w3c dawg SPARQL syntax tests" do
+    require 'dawg_test'
+
+    {
+      :syntax1  => Fixtures::SPARQL::Syn1,
+      :syntax2  => Fixtures::SPARQL::Syn2,
+      :syntax3  => Fixtures::SPARQL::Syn3,
+      :syntax4  => Fixtures::SPARQL::Syn4,
+      :syntax5  => Fixtures::SPARQL::Syn5,
+    }.each_pair do |suite, tests|
+      describe suite do
+        tests.each do |t|
+          specify "#{t.name}" do
+            #puts t.inspect
+            SPARQL::Grammar::Parser.new(Kernel.open(t.action))
+          end
+        end
+      end
+    end
+  end
+end
