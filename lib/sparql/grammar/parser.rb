03f50c7fb783cd08cf7239d2455a30e0e943ac0e
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-01-14 14:13:24 -0800

    Refactor token_productions to only need current production, rather than current and parent productions.

--- lib/sparql/grammar/parser.rb
+++ lib/sparql/grammar/parser.rb
@@ -24,6 +24,8 @@ module SPARQL; module Grammar
     #   the base URI to use when resolving relative URIs (for acessing intermediate parser productions)
     # @option options [#to_s]    :anon_base     ("gen0000")
     #   Basis for generating anonymous Nodes
+    # @option options [Boolean] :resolve_iris (false)
+    #   Resolve prefix and relative IRIs, otherwise output as symbols
     def initialize(input = nil, options = {})
       @options = {:anon_base => "gen0000"}.merge(options)
       self.input = input if input
@@ -240,10 +242,20 @@ module SPARQL; module Grammar
     # If entries are defined, pass production data to :start and/or :finish handlers
     def contexts(production)
       case production
+      when :BaseDecl
+        # [3]     BaseDecl      ::=       'BASE' IRI_REF
+        {
+          :finish => lambda { |data| self.base_uri = uri(data[:iri])}
+        }
       when :PrefixDecl
         # [4] PrefixDecl := 'PREFIX' PNAME_NS IRI_REF";
         {
-          :finish => lambda { |data| self.prefix(data[:prefix], data[:iri]) if data[:iri] }
+          :finish => lambda { |data|
+            if data[:iri]
+              self.prefix(data[:prefix], data[:iri])
+              add_prod_data(:prefix, [:prefix, data[:prefix], data[:iri]])
+            end
+          }
         }
       when :TriplesBlock
         # [21]    TriplesBlock ::= TriplesSameSubject ( '.' TriplesBlock? )?
@@ -395,66 +407,39 @@ module SPARQL; module Grammar
     end
 
     # Handlers for individual tokens based on production
-    def token_productions(parent_production, production)
-      case parent_production
-        # [3] BaseDecl ::= 'BASE' IRI_REF`
-      when :BaseDecl
-        case production
-        when :IRI_REF
-          lambda { |token| self.base_uri = uri(token) }
-        end
-      when :PrefixDecl
-        # [4] PrefixDecl := 'PREFIX' PNAME_NS IRI_REF";
-        case production
-        when :IRI_REF
-          lambda { |token| prod_data[:iri] = uri(self.base_uri, token) }
-        when :PNAME_NS
-          lambda { |token| prod_data[:prefix] = uri(token && token.to_sym) }
-        end
-      else
-        case production
-        when :a
-          # [37]    Verb ::= VarOrIRIref | 'a'
-          lambda { |token| add_prod_data(:Verb, RDF.type) }
-        when :VAR1, :VAR2
-          # [44]    Var ::= VAR1 | VAR2
-          lambda { |token| add_prod_data(:Var, RDF::Query::Variable.new(token)) }
-        when :BooleanLiteral
-          # [45]    GraphTerm ::= IRIref | RDFLiteral | NumericLiteral | BooleanLiteral | BlankNode | NIL
-          lambda { |token|
-            add_prod_data(:literal, RDF::Literal.new(token, :datatype => RDF::XSD.boolean))
-          }
-        when :LANGTAG
-          # [60]    RDFLiteral ::= String ( LANGTAG | ( '^^' IRIref ) )?
-          lambda { |token| add_prod_data(:language, token) }
-        when :INTEGER
-          # [62]    NumericLiteralUnsigned ::= INTEGER | DECIMAL | DOUBLE
-          lambda { |token| add_prod_data(:literal, RDF::Literal.new(token, :datatype => RDF::XSD.integer)) }
-        when :DECIMAL
-          # [62]    NumericLiteralUnsigned ::= INTEGER | DECIMAL | DOUBLE
-          lambda { |token| add_prod_data(:literal, RDF::Literal.new(token, :datatype => RDF::XSD.decimal)) }
-        when :DOUBLE
-          # [62]    NumericLiteralUnsigned ::= INTEGER | DECIMAL | DOUBLE
-          lambda { |token| add_prod_data(:literal, RDF::Literal.new(token, :datatype => RDF::XSD.double)) }
-        when :STRING_LITERAL1, :STRING_LITERAL2, :STRING_LITERAL_LONG1, :STRING_LITERAL_LONG2
-          # [66]    String ::= STRING_LITERAL1 | STRING_LITERAL2 | STRING_LITERAL_LONG1 | STRING_LITERAL_LONG2
-          lambda { |token| add_prod_data(:string, token) }
-        when :IRI_REF
-          # [67]    IRIref ::= IRI_REF | PrefixedName
-          lambda { |token| add_prod_data(:iri, uri(self.base_uri, token)) }
-        when :PNAME_NS
-          # [68]    PrefixedName ::= PNAME_LN | PNAME_NS
-          lambda { |token| add_prod_data(:PrefixedName, ns(nil, token)) }
-        when :PNAME_LN
-          # [68]    PrefixedName ::= PNAME_LN | PNAME_NS
-          lambda { |token| add_prod_data(:PrefixedName, ns(*token)) }
-        when :BLANK_NODE_LABEL
-          # [69]    BlankNode ::= BLANK_NODE_LABEL | ANON
-          lambda { |token| add_prod_data(:BlankNode, gen_node(token)) }
-        when :ANON
-          # [69]    BlankNode ::= BLANK_NODE_LABEL | ANON
-          lambda { |token| add_prod_data(:BlankNode, gen_node()) }
-        end
+    def token_productions(production)
+      case production
+      when :a
+        lambda { |token| add_prod_data(:Verb, RDF.type) }
+      when :ANON
+        lambda { |token| add_prod_data(:BlankNode, gen_node()) }
+      when :BLANK_NODE_LABEL
+        lambda { |token| add_prod_data(:BlankNode, gen_node(token)) }
+      when :BooleanLiteral
+        lambda { |token|
+          add_prod_data(:literal, RDF::Literal.new(token, :datatype => RDF::XSD.boolean))
+        }
+      when :DECIMAL
+        lambda { |token| add_prod_data(:literal, RDF::Literal.new(token, :datatype => RDF::XSD.decimal)) }
+      when :DOUBLE
+        lambda { |token| add_prod_data(:literal, RDF::Literal.new(token, :datatype => RDF::XSD.double)) }
+      when :INTEGER
+        lambda { |token| add_prod_data(:literal, RDF::Literal.new(token, :datatype => RDF::XSD.integer)) }
+      when :IRI_REF
+        lambda { |token| add_prod_data(:iri, uri(self.base_uri, token)) }
+      when :LANGTAG
+        lambda { |token| add_prod_data(:language, token) }
+      when :PNAME_LN
+        lambda { |token| add_prod_data(:PrefixedName, ns(*token)) }
+      when :PNAME_NS
+        lambda { |token|
+          add_prod_data(:PrefixedName, ns(nil, token))    # [68]    PrefixedName ::= PNAME_LN | PNAME_NS
+          prod_data[:prefix] = uri(token && token.to_sym) # [4] PrefixDecl := 'PREFIX' PNAME_NS IRI_REF";
+        }
+      when :STRING_LITERAL1, :STRING_LITERAL2, :STRING_LITERAL_LONG1, :STRING_LITERAL_LONG2
+        lambda { |token| add_prod_data(:string, token) }
+      when :VAR1, :VAR2       # [44]    Var ::= VAR1 | VAR2
+        lambda { |token| add_prod_data(:Var, RDF::Query::Variable.new(token)) }
       end
     end
     
@@ -462,7 +447,7 @@ module SPARQL; module Grammar
     def onToken(prod, token)
       unless @productions.empty?
         parentProd = @productions.last
-        token_production = token_productions(parentProd.to_sym, prod.to_sym)
+        token_production = token_productions(prod.to_sym)
         if token_production
           progress("#{parentProd}(:token)", "#{token}: #{$verbose ? prod_data.inspect : prod_data.keys.inspect}")
           token_production.call(token)
@@ -501,45 +486,10 @@ module SPARQL; module Grammar
       $stderr.puts("[#{@lineno}]#{' ' * @productions.length}#{node}: #{message}") if $verbose
     end
 
-    # [5] SelectQuery ::= 'SELECT' ( 'DISTINCT' | 'REDUCED' )? ( Var+ | '*' ) DatasetClause* WhereClause
-    # [6] ConstructQuery ::= 'CONSTRUCT' ConstructTemplate DatasetClause* WhereClause SolutionModifier
-    # [7] DescribeQuery ::= 'DESCRIBE' ( VarOrIRIref+ | '*' ) DatasetClause* WhereClause? SolutionModifier
-    # `[8 AskQuery ::= 'ASK' DatasetClause* WhereClause
-    # [9] DatasetClause ::= 'FROM' (DefaultGraphClause | NamedGraphClause)
-    # [10] DefaultGraphClause ::= SourceSelector
-    # [11] NamedGraphClause ::= 'NAMED' SourceSelector`
-    # [12]    SourceSelector ::= IRIref
-    # [13]    WhereClause ::= 'WHERE'? GroupGraphPattern
-    # [14]    SolutionModifier ::= OrderClause? LimitOffsetClauses?
-    # [15]    LimitOffsetClauses ::= ( LimitClause OffsetClause? | OffsetClause LimitClause? )
-    # [16]    OrderClause ::= 'ORDER' 'BY' OrderCondition+
-    # [17]    OrderCondition ::= ( ( ASC' | 'DESC' ) BrackettedExpression )
-    #                          | ( Constraint | Var )
-    # [18]    LimitClause ::= 'LIMIT' INTEGER
-    # [19]    OffsetClause ::= 'OFFSET' INTEGER
-    # [20]    GroupGraphPattern ::= '{' TriplesBlock? ( ( GraphPatternNotTriples | Filter ) '.'? TriplesBlock? )* '}'
-    # [21]    TriplesBlock ::= TriplesSameSubject ( '.' TriplesBlock? )?
-    # [22]    GraphPatternNotTriples ::= OptionalGraphPattern | GroupOrUnionGraphPattern | GraphGraphPattern
-    # [23]    OptionalGraphPattern ::= 'OPTIONAL' GroupGraphPattern
-    # [24]    GraphGraphPattern ::= 'GRAPH' VarOrIRIref GroupGraphPattern
-    # [25]    GroupOrUnionGraphPattern ::= GroupGraphPattern ( 'UNION' GroupGraphPattern )*
-    # [26]    Filter ::= 'FILTER' Constraint
-    # [27]    Constraint ::= BrackettedExpression | BuiltInCall | FunctionCall
-    # [28]    FunctionCall ::= IRIref ArgList
-    # [29]    ArgList ::= ( NIL | '(' Expression ( ',' Expression )* ')' )
-    # [30]    ConstructTemplate ::= '{' ConstructTriples? '}'
-    # [31]    ConstructTriples ::= TriplesSameSubject ( '.' ConstructTriples? )?
-    # [32]    TriplesSameSubject ::= VarOrTerm PropertyListNotEmpty | TriplesNode PropertyList
-    # [33]    PropertyListNotEmpty ::= Verb ObjectList ( ';' ( Verb ObjectList )? )*
-    # [34]    PropertyList ::= PropertyListNotEmpty?
-    # [35]    ObjectList ::= Object ( ',' Object )*
-    # [36]    Object ::= GraphNode
-    # [37]    Verb ::= VarOrIRIref | 'a'
-    # [38]    TriplesNode ::= Collection | BlankNodePropertyList
-    # [39]    BlankNodePropertyList ::= '[' PropertyListNotEmpty ']'
     # [40]    Collection ::= '(' GraphNode+ ')'
     #
     # Take collection of objects and create RDF Collection using rdf:first, rdf:rest and rdf:nil
+    # @param [Hash] data Production Data
     def expand_collection(data)
       # Add any triples generated from deeper productions
       add_prod_data(:triple, data[:triple])
@@ -561,73 +511,6 @@ module SPARQL; module Grammar
       end
       add_triple(:Collection, :subject => first, :predicate => RDF["rest"], :object => RDF["nil"])
     end
-
-    # [41]    GraphNode ::= VarOrTerm | TriplesNode
-    # [42]    VarOrTerm ::= Var | GraphTerm
-    # [43]    VarOrIRIref ::= Var | IRIref
-    # [44]    Var ::= VAR1 | VAR2
-    # [46]    Expression ::= ConditionalOrExpression
-    # [47]    ConditionalOrExpression ::= ConditionalAndExpression ( '||' ConditionalAndExpression )*
-    # [48]    ConditionalAndExpression ::= ValueLogical ( '&&' ValueLogical )*
-    # [49]    ValueLogical ::= RelationalExpression
-    # [50]    RelationalExpression ::= NumericExpression ( '=' NumericExpression | '!=' NumericExpression | '<' NumericExpression | '>' NumericExpression | '<=' NumericExpression | '>=' NumericExpression )?
-    # [51]    NumericExpression ::= AdditiveExpression
-    # [52]    AdditiveExpression ::= MultiplicativeExpression ( '+' MultiplicativeExpression | '-' MultiplicativeExpression | NumericLiteralPositive | NumericLiteralNegative )*
-    # [53]    MultiplicativeExpression ::= UnaryExpression ( '*' UnaryExpression | '/' UnaryExpression )*
-    # [54]    UnaryExpression ::= '!' PrimaryExpression
-    #                           | '+' PrimaryExpression
-    #                           | '-' PrimaryExpression
-    #                           | PrimaryExpression
-    # [55]    PrimaryExpression ::= BrackettedExpression | BuiltInCall | IRIrefOrFunction | RDFLiteral | NumericLiteral | BooleanLiteral | Var
-    # [56]    BrackettedExpression ::= '(' Expression ')'
-    # [57]    BuiltInCall ::= 'STR' '(' Expression ')'
-    #                       | 'LANG' '(' Expression ')'
-    #                       | 'LANGMATCHES' '(' Expression ',' Expression ')'
-    #                       | 'DATATYPE' '(' Expression ')'
-    #                       | 'BOUND' '(' Var ')'
-    #                       | 'sameTerm' '(' Expression ',' Expression ')'
-    #                       | 'isIRI' '(' Expression ')'
-    #                       | 'isURI' '(' Expression ')'
-    #                       | 'isBLANK' '(' Expression ')'
-    #                       | 'isLITERAL' '(' Expression ')'
-    #                       | RegexExpression
-    # [58]    RegexExpression ::= 'REGEX' '(' Expression ',' Expression ( ',' Expression )? ')'
-    # [59]    IRIrefOrFunction ::= IRIref ArgList?
-    # [61]    NumericLiteral ::= NumericLiteralUnsigned | NumericLiteralPositive | NumericLiteralNegative
-    # [63]    NumericLiteralPositive ::= INTEGER_POSITIVE | DECIMAL_POSITIVE | DOUBLE_POSITIVE
-    # [64]    NumericLiteralNegative ::= INTEGER_NEGATIVE | DECIMAL_NEGATIVE | DOUBLE_NEGATIVE
-    # [65]    BooleanLiteral ::= 'true' | 'false'
-    # [70]    IRI_REF  ::= '<' ([^<>"{}|^`\]-[#x00-#x20])* '>'
-    # [71]    PNAME_NS ::= PN_PREFIX? ':'
-    # [72]    PNAME_LN ::= PNAME_NS PN_LOCAL
-    # [73]    BLANK_NODE_LABEL ::= '_:' PN_LOCAL
-    # [74]    VAR1 ::= '?' VARNAME
-    # [75]    VAR2 ::= '$' VARNAME
-    # [76]    LANGTAG ::= '@' [a-zA-Z]+ ('-' [a-zA-Z0-9]+)*
-    # [77]    INTEGER ::= [0-9]+
-    # [78]    DECIMAL ::= [0-9]+ '.' [0-9]* | '.' [0-9]+
-    # [79]    DOUBLE ::= [0-9]+ '.' [0-9]* EXPONENT | '.' ([0-9])+ EXPONENT | ([0-9])+ EXPONENT
-    # [80]    INTEGER_POSITIVE ::= '+' INTEGER
-    # [81]    DECIMAL_POSITIVE ::= '+' DECIMAL
-    # [82]    DOUBLE_POSITIVE ::= +' DOUBLE
-    # [83]    INTEGER_NEGATIVE ::= '-' INTEGER
-    # [84]    DECIMAL_NEGATIVE ::= '-' DECIMAL
-    # [85]    DOUBLE_NEGATIVE ::= '-' DOUBLE
-    # [86]    EXPONENT ::= [eE] [+-]? [0-9]+
-    # [87]    STRING_LITERAL1 ::= "'" ( ([^#x27#x5C#xA#xD]) | ECHAR )* "'"
-    # [88]    STRING_LITERAL2 ::= '"' ( ([^#x22#x5C#xA#xD]) | ECHAR )* '"'
-    # [89]    STRING_LITERAL_LONG1 ::= "'''" ( ( "'" | "''" )? ( [^'\] | ECHAR ) )* "'''"
-    # [90]    STRING_LITERAL_LONG2 ::= '"""' ( ( '"' | '""' )? ( [^"\] | ECHAR ) )* '"""'
-    # [91]    ECHAR ::= '\' [tbnrf\"']
-    # [92]    NIL ::= '(' WS* ')'
-    # [93]    WS ::= #x20 | #x9 | #xD | #xA
-    # [94]    ANON ::= '[' WS* ']'
-    # [95]    PN_CHARS_BASE ::= [A-Z] | [a-z] | [#x00C0-#x00D6] | [#x00D8-#x00F6] | [#x00F8-#x02FF] | [#x0370-#x037D] | [#x037F-#x1FFF] | [#x200C-#x200D] | [#x2070-#x218F] | [#x2C00-#x2FEF] | [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] | [#x10000-#xEFFFF]
-    # [96]    PN_CHARS_U ::= PN_CHARS_BASE | '_'
-    # [97]    VARNAME ::= ( PN_CHARS_U | [0-9] ) ( PN_CHARS_U | [0-9] | #x00B7 | [#x0300-#x036F] | [#x203F-#x2040] )*
-    # [98]    PN_CHARS ::= PN_CHARS_U | '-' | [0-9] | #x00B7 | [#x0300-#x036F] | [#x203F-#x2040]
-    # [99]    PN_PREFIX ::= PN_CHARS_BASE ((PN_CHARS|'.')* PN_CHARS)?
-    # [100]   PN_LOCAL ::= ( PN_CHARS_U | [0-9] ) ((PN_CHARS|'.')* PN_CHARS)?
 
   private
 
