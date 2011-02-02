e58a840cd4c0e8e80a299d9e01b38b5e58ac1643
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-02-01 17:02:06 -0800

    Add RSpec setup to allow focus.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,23 @@ require 'rdf/ntriples'
 
 RSpec.configure do |config|
   config.include(RDF::Spec::Matchers)
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  config.exclusion_filter = {:ruby => lambda { |version|
+    RUBY_VERSION.to_s !~ /^#{version}/
+  }}
 end
 
 DAWG = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#')
 MF   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#')
 QT   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-query#')
 RS   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/result-set#')
+
+class RDF::Query
+  ##
+  # @param  [Object] other
+  # @return [Boolean]
+  def ==(other)
+    other.is_a?(RDF::Query) && patterns == other.patterns
+  end
+end
