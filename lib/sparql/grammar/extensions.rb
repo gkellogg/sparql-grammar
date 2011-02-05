437747d93e36164a33de6cf1bfcef4fecaa9cc34
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-02-05 10:59:59 -0800

    Add GroupQuery#filter and GroupQuery#execute as boilerplate.

--- lib/sparql/grammar/extensions.rb
+++ lib/sparql/grammar/extensions.rb
@@ -130,6 +130,12 @@ module RDF
     attr_accessor :operation
 
     ##
+    # Filter to be applied to solution set.
+    # Represented as an S-Expression of SPARQL Algebra operations
+    # @return [Array]
+    attr_accessor :filter
+
+    ##
     # Initializes a new algebra query.
     #
     # @param  [Array<RDF::Query>] queries
@@ -192,6 +198,50 @@ module RDF
     # Query has no queries
     def empty?
       queries.empty?
+    end
+    
+    ##
+    # Executes this query on the given `queryable` graph or repository.
+    #
+    # @param  [RDF::Queryable] queryable
+    #   the graph or repository to query
+    # @param  [Hash{Symbol => Object}] options
+    #   any additional keyword options
+    # @return [RDF::Query::Solutions]
+    #   the resulting solution sequence
+    # @see    http://www.w3.org/TR/rdf-sparql-query/#sparqlAlgebra
+    #
+    #     D : a dataset
+    #     D(G) : D a dataset with active graph G (the one patterns match against)
+    #     D[i] : The graph with IRI i in dataset D
+    #     D[DFT] : the default graph of D
+    #     P, P1, P2 : graph patterns
+    #     L : a solution sequence
+    def execute(queryable, options = {})
+      case @operation
+      when :union
+        # Definition: Union
+        # Let Ω1 and Ω2 be multisets of solution mappings. We define:
+        # Union(Ω1, Ω2) = { μ | μ in Ω1 or μ in Ω2 }
+        # card[Union(Ω1, Ω2)](μ) = card[Ω1](μ) + card[Ω2](μ)
+        # eval(D(G), Union(P1,P2)) = Union(eval(D(G), P1), eval(D(G), P2)
+      when :leftjoin
+        # Let Ω1 and Ω2 be multisets of solution mappings and expr be an expression. We define:
+        # LeftJoin(Ω1, Ω2, expr) = Filter(expr, Join(Ω1, Ω2)) set-union Diff(Ω1, Ω2, expr)
+        # card[LeftJoin(Ω1, Ω2, expr)](μ) = card[Filter(expr, Join(Ω1, Ω2))](μ) + card[Diff(Ω1, Ω2, expr)](μ)
+        #
+        # Written in full that is:
+        # LeftJoin(Ω1, Ω2, expr) =
+        #     { merge(μ1, μ2) | μ1 in Ω1and μ2 in Ω2, and μ1 and μ2 are compatible and expr(merge(μ1, μ2)) is true }
+        # set-union
+        #     { μ1 | μ1 in Ω1and μ2 in Ω2, and μ1 and μ2 are not compatible }
+        # set-union
+        #     { μ1 | μ1 in Ω1and μ2 in Ω2, and μ1 and μ2 are compatible and expr(merge(μ1, μ2)) is false }
+        # eval(D(G), LeftJoin(P1, P2, F)) = LeftJoin(eval(D(G), P1), eval(D(G), P2), F)
+      else #join
+        # Join(Ω1, Ω2) = { merge(μ1, μ2) | μ1 in Ω1and μ2 in Ω2, and μ1 and μ2 are compatible }
+        # eval(D(G), Join(P1, P2)) = Join(eval(D(G), P1), eval(D(G), P2))
+      end
     end
 
     def inspect
