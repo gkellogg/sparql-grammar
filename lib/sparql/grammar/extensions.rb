b78d9078e2e7da72d62f475668efd204ee613ee0
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-02-11 21:13:20 +0100

    Move #to_sxp to RDF classes for SXP.

--- lib/sparql/grammar/extensions.rb
+++ lib/sparql/grammar/extensions.rb
@@ -2,53 +2,11 @@
 
 module RDF
   class URI
-    ##
-    # Returns the SXP representation of this object.
-    #
-    # @return [String]
-    def to_sxp; qname || "<#{self}>"; end
-    
     # Override qname to save value for SXP serialization
     def qname=(value); @qname = value; end
     def qname; @qname; end
   end
 
-  class Literal
-    ##
-    # Returns the SXP representation of a Literal.
-    #
-    # @return [String]
-    def to_sxp
-      case datatype
-      when XSD.boolean, XSD.integer, XSD.double, XSD.decimal, XSD.time
-        object.to_sxp
-      else
-        text = value.dump
-        text << "@#{language}" if self.has_language?
-        text << "^^#{datatype.to_sxp}" if self.has_datatype?
-        text
-      end
-    end
-  end
-
-  class Statement
-    def inspect
-      to_sxa.map(&:to_sxp).inspect
-    end
-    
-    # Transform Query into an Array form of an SXP
-    # @return [Statement]
-    def to_sxa
-      [:triple, subject, predicate, object]
-    end
-
-    # Transform Statement into an SXP
-    # @return [String]
-    def to_sxp
-      to_sxa.to_sxp
-    end
-  end
-  
   class Query
     ##
     # @return [RDF::Resource]
@@ -91,22 +49,6 @@ module RDF
 
     def inspect
       "RDF::Query(#{context ? context.to_sxp : 'nil'})#{patterns.inspect}"
-    end
-
-    # Transform Query into an Array form of an SXP
-    #
-    # If Query is named, it's treated as a GroupGraphPattern, otherwise, a BGP
-    #
-    # @return [Array]
-    def to_sxa
-      res = [:bgp] + patterns
-      named? ? [:graph, context, res] : res
-    end
-    
-    # Transform Query into an SXP
-    # @return [String]
-    def to_sxp
-      to_sxa.to_sxp
     end
   end
   
