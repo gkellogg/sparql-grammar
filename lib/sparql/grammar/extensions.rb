117e2d11b161a1eba06eb765f82eb58d114dd728
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-02-21 12:47:41 -0800

    RDF extensions moved to RDF.rb

--- lib/sparql/grammar/extensions.rb
+++ lib/sparql/grammar/extensions.rb
@@ -7,51 +7,6 @@ module RDF
     def qname; @qname; end
   end
 
-  class Query
-    ##
-    # @return [RDF::Resource]
-    attr_accessor :context
-
-    # Add patterns from another query to form a new Query
-    # @param [RDF::Query] other
-    # @return [RDF::Query]
-    def +(other)
-      Query.new(self.patterns + other.patterns)
-    end
-    
-    # Is this is a named query?
-    # @return [Boolean]
-    def named?
-      !unnamed?
-    end
-    
-    def unnamed?
-      options[:context].nil?
-    end
-    
-    # Add name to query
-    # @param [RDF::Value] value
-    # @return [RDF::Value]
-    def context=(value)
-      options[:context] = value
-    end
-    
-    # Name of this query, if any
-    # @return [RDF::Value]
-    def context
-      options[:context]
-    end
-
-    # Query has no patterns
-    def empty?
-      patterns.empty?
-    end
-
-    def inspect
-      "RDF::Query(#{context ? context.to_sxp : 'nil'})#{patterns.inspect}"
-    end
-  end
-  
   ##
   # A Query sub-class to represent :join, :union and :leftjoin
   #
