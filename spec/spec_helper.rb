72df7e851c0253f5e1ede9ab69a98dd867f88aa2
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-03-07 16:48:54 -0800

    Support for open-uri/cached.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,28 @@
+require "bundler/setup"
+require 'open-uri/cached'
 require 'sparql/grammar'
 require 'rdf/spec'
 require 'rdf/ntriples'
 require 'rdf/n3'
 
 RSpec.configure do |config|
-  config.include(RDF::Spec::Matchers)
+  #config.include(RDF::Spec::Matchers)
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
-  config.exclusion_filter = {:ruby => lambda { |version|
-    RUBY_VERSION.to_s !~ /^#{version}/
-  }}
+  config.exclusion_filter = {
+    :ruby           => lambda { |version| RUBY_VERSION.to_s !~ /^#{version}/},
+    :blank_nodes    => 'unique',
+    :arithmetic     => 'native',
+    :sparql_algebra => false,
+    #:status         => 'bug',
+    :reduced        => 'all',
+  }
 end
+
+# Create and maintain a cache of downloaded URIs
+URI_CACHE = File.expand_path(File.join(File.dirname(__FILE__), "uri-cache"))
+Dir.mkdir(URI_CACHE) unless File.directory?(URI_CACHE)
+OpenURI::Cache.class_eval { @cache_path = URI_CACHE }
 
 DAWG = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#')
 MF   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#')
