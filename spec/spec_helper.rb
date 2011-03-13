d9cb2c6ac174f186e3e54c126b0e2b87528561be
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-03-13 13:33:16 -0700

    Added sparql_query to spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 require "bundler/setup"
 require 'open-uri/cached'
 require 'sparql/grammar'
+require 'sparql/spec'
 require 'rdf/spec'
 require 'rdf/ntriples'
 require 'rdf/n3'
+Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   #config.include(RDF::Spec::Matchers)
@@ -29,15 +31,67 @@ MF   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-mani
 QT   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-query#')
 RS   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/result-set#')
 
-class RDF::Query
-  ##
-  # @param  [Object] other
-  # @return [Boolean]
-  def ==(other)
-    other.is_a?(RDF::Query) && patterns == other.patterns && context == other.context
+# This file defines the sparql query function, which makes a sparql query and returns results.
+
+# run a sparql query against SPARQL S-Expression (SSE)
+# Options:
+#   :graphs
+#     example
+#       opts[:graphs] ==
+#        { :default => {
+#             :data => '...',
+#             :format => :ttl
+#           },
+#           <g1> => {
+#            :data => '...',
+#            :format => :ttl
+#           }
+#           <g2> => {
+#            :default => true
+#           }
+#        }
+#   :allow_empty => true
+#     allow no data for query (raises an exception by default)
+#   :query
+#     A SPARQL query, as a string
+#   :repository
+#     The dydra repository associated with the account to use
+#   :form
+#     :ask, :construct, :select or :describe
+def sparql_query(opts)
+  opts[:to_hash] = true unless opts.has_key?(:to_hash)
+  raise "A query is required to be run" if opts[:query].nil?
+
+  # Load default and named graphs into repository
+  repo = RDF::Repository.new do |r|
+    opts[:graphs].each do |key, info|
+      next if key == :result
+      data, format, default = info[:data], info[:format], info[:default]
+      if data
+        RDF::Reader.for(:file_extension => format).new(data).each_statement do |st|
+          st.context = key unless key == :default || default
+          r << st
+        end
+      end
+    end
   end
 
-  def inspect
-    "RDF::Query(#{context ? context.to_sxp : 'nil'})#{patterns.inspect}"
+  query_str = opts[:query]
+  query_opts = {:debug => ENV['PARSER_DEBUG']}
+  query_opts[:base_uri] = opts[:base_uri]
+  
+  if opts[:sse]
+    query = SPARQL::Algebra.parse(query_str, query_opts)
+  else
+    query = SPARQL::Grammar.parse(query_str, query_opts)
+  end
+
+  case opts[:form]
+  when :ask, :describe, :construct
+    query.execute(repo, :debug => ENV['EXEC_DEBUG'])
+  else
+    results = query.execute(repo, :debug => ENV['EXEC_DEBUG'])
+    opts[:to_hash] ? results.map(&:to_hash) : results
   end
 end
+
