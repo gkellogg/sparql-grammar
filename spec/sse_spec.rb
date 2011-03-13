3040e1421007929b37bcacf73d1e585acc904249
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-03-13 13:33:49 -0700

    Test both lexical and object translation to SSE. Lexical tests that fail are made pending.

--- spec/sse_spec.rb
+++ spec/sse_spec.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 require 'sparql/spec'
 
 describe SPARQL::Grammar::Parser do
-  describe "w3c dawg SPARQL evaluation tests" do
+  describe "w3c dawg SPARQL tests to SSE" do
     SPARQL::Spec.load_sparql1_0_tests.group_by(&:manifest).each do |man, tests|
       describe man.to_s.split("/")[-2] do
         tests.each do |t|
@@ -13,6 +13,21 @@ describe SPARQL::Grammar::Parser do
               query = SPARQL::Grammar.parse(t.action.query_string)
               sse = SPARQL::Algebra.parse(t.action.sse_string)
               query.should == sse
+            end
+
+            it "parses #{t.name} to lexically equivalent SSE" do
+              query = SPARQL::Grammar.parse(t.action.query_string)
+              normalized_query = query.to_sxp.
+                gsub(/\s+/m, " ").
+                gsub(/\(\s+\(/, '((').
+                gsub(/\)\s+\)/, '))').
+                strip
+              normalized_result = t.action.sse_string.
+                gsub(/\s+/m, " ").
+                gsub(/\(\s+\(/, '((').
+                gsub(/\)\s+\)/, '))').
+                strip
+              normalized_query.should == normalized_result rescue pending("Lexical equivalence")
             end
           else
             it "??? #{t.name}" do
